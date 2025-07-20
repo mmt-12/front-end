329bc76c8eb076fd85f322b06f3a254fb3ee9d4c
wndgur2 <wndgur76@naver.com>
2025-07-20 13:05:02 +0900

    refactor: path alias 사용

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,5 +1,5 @@
 import '@emotion/react'
-import { ThemeType } from '../styles/theme'
+import { ThemeType } from '@/styles/theme'
 
 declare module '@emotion/react' {
   export interface Theme extends ThemeType {}
