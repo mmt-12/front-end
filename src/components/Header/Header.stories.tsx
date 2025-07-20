2cc1721e846643e302574e4bcbec1301968ba36a
wndgur2 <wndgur76@naver.com>
2025-07-20 13:41:49 +0900

    docs: Header story 작성

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Header from '.'
+import { useHeaderStore } from '@/store/headerStore'
+import { useEffect } from 'react'
+import { Bell, UsersGroupRounded } from '@solar-icons/react'
+
+const meta = {
+  title: 'components/Header',
+  component: Header,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Header>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    routeName: '페이지 제목',
+    leftItem: {
+      icon: <UsersGroupRounded size={24} weight='Bold' />, // Replace with actual icon component
+      onClick: () => console.log('Left clicked'),
+    },
+    rightItem: {
+      icon: <Bell size={24} weight='Bold' />, // Replace with actual icon component
+      onClick: () => console.log('Right clicked'),
+    },
+  },
+  render: args => {
+    const { setRouteName, setLeftItem, setRightItem } = useHeaderStore(
+      state => state,
+    )
+    useEffect(() => {
+      setRouteName('페이지 제목')
+      setLeftItem({
+        icon: <UsersGroupRounded size={24} weight='Bold' />, // Replace with actual icon component
+        onClick: () => console.log('Left clicked'),
+      })
+      setRightItem({
+        icon: <Bell size={24} weight='Bold' />, // Replace with actual icon component
+        onClick: () => console.log('Right clicked'),
+      })
+    }, [])
+    return <Header {...args} />
+  },
+}
