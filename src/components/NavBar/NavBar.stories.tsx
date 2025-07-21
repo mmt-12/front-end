4caa8e5da135608d2d5d094dfb324810ec6f138e
wndgur2 <wndgur76@naver.com>
2025-07-21 18:08:58 +0900

    fix: react-router addon 최신으로 변경 (deprecated for ts)

--- src/components/NavBar/NavBar.stories.tsx
+++ src/components/NavBar/NavBar.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import NavBar from '.'
-import { withRouter } from 'storybook-addon-react-router-v6'
 import { ROUTES } from '@/routes/ROUTES'
+import { withRouter } from 'storybook-addon-remix-react-router'
 
 const meta = {
   title: 'components/NavBar',
