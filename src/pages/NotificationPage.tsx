1f66447185f5ad8139e8060deb3083f3d6d29d90
junghyunl <fnsk1256@naver.com>
2025-07-22 23:46:00 +0900

    fix: 알림함 헤더 오른쪽 아이템 삭제

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -3,6 +3,9 @@ import useHeader from '@/hooks/useHeader'
 export default function NotificationPage() {
   useHeader({
     routeName: '알림함',
+    rightItem: {
+      icon: null,
+    },
   })
 
   return <div>notification page</div>
