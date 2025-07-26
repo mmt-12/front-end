e42e07a67c51dfc2330e4a610db00220b4314eab
junghyunl <fnsk1256@naver.com>
2025-07-27 04:51:27 +0900

    chore: 스토리북 뷰포트 추가

---
+++ .storybook/style.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react'
+
+export const storybookStyle = css`
+  .sb-show-main.sb-main-padded {
+    padding: 0 !important;
+  }
+
+  html {
+    max-width: none !important;
+    margin: 0 !important;
+    background-color: transparent !important;
+  }
+`
