258f8ab8429f6f602f69130b7df06a5343265d3c
wndgur2 <wndgur76@naver.com>
2025-07-27 13:38:56 +0900

    feat: 프로필 컴포넌트 개선

--- src/components/common/Profile/Profile.tsx
+++ src/components/common/Profile/Profile.tsx
@@ -1,45 +1,73 @@
 import { css, type Theme } from '@emotion/react'
 import Badge from '../Badge'
+import defaultImageUrl from '@/assets/mascot/default-profile.png'
 
 export interface Props {
-  id: string
   name: string
   imageUrl?: string
   badgeId?: number
   description?: string
 }
 
-export default function Profile(props: Props) {
+export default function Profile({
+  name,
+  imageUrl,
+  badgeId,
+  description,
+}: Props) {
   return (
     <div css={containerStyle}>
-      <img src={props.imageUrl} alt={props.name} />
-      <h2>{props.name}</h2>
-      {props.description && <p>{props.description}</p>}
-      {props.badgeId && <Badge id={props.badgeId} />}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={name}
+          onError={e => {
+            e.currentTarget.src = defaultImageUrl
+          }}
+          css={imageStyle}
+        />
+      )}
+      <div css={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+        <div css={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <h2 css={nameStyle}>{name}</h2>
+          {badgeId && <Badge id={badgeId} />}
+        </div>
+        {description && <p css={descriptionStyle}>{description}</p>}
+      </div>
     </div>
   )
 }
 
-const containerStyle = (theme: Theme) =>
+const containerStyle = css({
+  padding: 8,
+  display: 'inline-flex',
+  alignItems: 'center',
+  gap: '10px',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+})
+
+const nameStyle = css({
+  margin: 0,
+  fontSize: '16px',
+  fontWeight: 'bold',
+})
+
+const imageStyle = css({
+  width: '64px',
+  height: '64px',
+  flexShrink: 0,
+  borderRadius: '50%',
+  objectFit: 'cover',
+})
+
+const descriptionStyle = (theme: Theme) =>
   css({
-    padding: 8,
-    display: 'inline-flex',
-    alignItems: 'center',
-    gap: '8px',
-    img: {
-      width: '40px',
-      height: '40px',
-      borderRadius: '50%',
-      objectFit: 'cover',
-    },
-    h2: {
-      margin: 0,
-      fontSize: '16px',
-      fontWeight: 'bold',
-    },
-    p: {
-      margin: 0,
-      fontSize: '14px',
-      color: theme.stone[500],
-    },
+    overflow: 'hidden',
+    width: '100%',
+    margin: 0,
+    fontSize: '14px',
+    color: theme.stone[500],
+    textOverflow: 'ellipsis',
+    fontFamily: 'PFStardust',
   })
