d7a3e141ace69dbb77ab8262206a973a11c85796
wndgur2 <wndgur76@naver.com>
2025-07-27 15:23:54 +0900

    docs: 접근성, 테스트 addon 추가

--- src/components/common/NavBar/NavBar.stories.tsx
+++ src/components/common/NavBar/NavBar.stories.tsx
@@ -1,34 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import NavBar from '.'
-import { ROUTES } from '@/routes/ROUTES'
 import { withRouter } from 'storybook-addon-remix-react-router'
+import { expect } from 'storybook/test'
 
 const meta = {
   title: 'components/NavBar',
   component: NavBar,
   parameters: {
     reactRouter: {
-      routePath: ROUTES.MEMORY_LIST,
+      routePath: '*',
     },
   },
-  decorators: [
-    withRouter,
-    Story => (
-      <div style={{ pointerEvents: 'none' }}>
-        <Story />
-      </div>
-    ),
-  ],
+  decorators: [withRouter],
+  play: async ({ canvas, userEvent }) => {
+    // Click on a button and assert that a dialog appears
+    const button = canvas.getByRole('button')
+    await userEvent.click(button)
+    const buttons = canvas.getAllByRole('button')
+    await expect(buttons).toHaveLength(5)
+    await expect(buttons[1]).toHaveTextContent('기억')
+    await expect(buttons[2]).toHaveTextContent('이모티콘')
+    await expect(buttons[3]).toHaveTextContent('약속')
+    await expect(buttons[4]).toHaveTextContent('보이스')
+  },
 } satisfies Meta<typeof NavBar>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  parameters: {
-    reactRouter: {
-      routePath: ROUTES.MEMORY_LIST,
-    },
-  },
-}
+export const Default: Story = {}
