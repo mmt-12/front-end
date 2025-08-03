c295021ff1d50a3af5c5c1de582173a6b423a2d4
wndgur2 <wndgur76@naver.com>
2025-08-03 11:24:15 +0900

    fix: 훅 외부에서 item을 동적으로 변경할 수 있도록 수정

--- src/pages/MemoryListPage.tsx
+++ src/pages/MemoryListPage.tsx
@@ -1,19 +1,33 @@
 import GreetingPopup from '@/components/common/popup/GreetingPopup'
 import useHeader from '@/hooks/useHeader'
-import { Bell, SortByTime } from '@solar-icons/react'
+import { IHeaderItem } from '@/types'
+import { Album, SortByTime } from '@solar-icons/react'
+import { useEffect, useState } from 'react'
 
 export default function MemoryListPage() {
+  const [isGrid, setIsGrid] = useState(false)
+  const [leftItem, setLeftItem] = useState<IHeaderItem>({
+    icon: SortByTime,
+    onClick: () => toggleViewMode(),
+  })
+
   useHeader({
     routeName: '기억',
-    leftItem: {
-      icon: SortByTime,
-      onClick: () => console.log('list clicked'),
-    },
-    rightItem: {
-      icon: Bell,
-      onClick: () => console.log('Bell clicked'),
-    },
+    leftItem: leftItem,
   })
+
+  const toggleViewMode = () => {
+    setIsGrid(isGrid => !isGrid)
+  }
+
+  useEffect(() => {
+    if (!setLeftItem) return
+
+    setLeftItem({
+      icon: isGrid ? Album : SortByTime,
+      onClick: () => toggleViewMode(),
+    })
+  }, [isGrid, setLeftItem])
 
   return (
     <div>
