c3239cd51d8e7a9bf5154b3ba570ad0ddcf9b0b1
junghyunl <fnsk1256@naver.com>
2025-08-10 18:24:08 +0900

    fix: 스토리 수정

--- src/components/guest-book/MbtiChart/MbtiChart.stories.tsx
+++ src/components/guest-book/MbtiChart/MbtiChart.stories.tsx
@@ -1,13 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import MbtiChart from './MbtiChart'
-import { withRouter } from 'storybook-addon-remix-react-router'
 
 const meta = {
   title: 'components/guest book/MbtiChart',
   component: MbtiChart,
   tags: ['autodocs'],
-
-  decorators: [withRouter],
+  decorators: [
+    Story => (
+      <div style={{ width: 200, height: 200 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof MbtiChart>
 
 export default meta
