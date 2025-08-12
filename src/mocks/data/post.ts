91df51397c0c243d3196d87caf66d14c5de5458a
wndgur2 <wndgur76@naver.com>
2025-08-12 12:22:05 +0900

    fix: mock data 수정

--- src/mocks/data/post.ts
+++ src/mocks/data/post.ts
@@ -1,10 +1,10 @@
 export const POST = {
   id: '1',
   images: [
-    'test_images/image1.png',
-    'test_images/image1.png',
-    'test_images/image1.png',
-    'test_images/image1.png',
+    '/test_images/image1.png',
+    '/test_images/image2.png',
+    '/test_images/image3.png',
+    '/test_images/image4.png',
   ],
   content: '이것은 첫 번째 포스트의 내용입니다.',
   author: {
@@ -12,5 +12,5 @@ export const POST = {
     name: '홍길동',
     profileImage: 'test_images/profile1.png',
   },
-  createdAt: '2023-01-01T12:00:00Z',
+  createdAt: new Date('2023-01-01T12:00:00Z'),
 }
