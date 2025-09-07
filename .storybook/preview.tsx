4dd050d2f78439d906ba346991d6e828b2538a5c
junghyunl <fnsk1256@naver.com>
2025-09-07 19:58:01 +0900

    fix: 스토리북 에러 수정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import { Global, ThemeProvider } from '@emotion/react'
 import type { Preview } from '@storybook/react-vite'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { APIProvider } from '@vis.gl/react-google-maps'
 import { INITIAL_VIEWPORTS } from 'storybook/viewport'
 
@@ -7,6 +8,8 @@ import { ModalProvider } from '../src/contexts/ModalProvider'
 import { globalStyle } from '../src/styles/global'
 import { theme } from '../src/styles/theme'
 import { storybookStyle } from './style.ts'
+
+const queryClient = new QueryClient()
 
 const preview: Preview = {
   parameters: {
@@ -30,9 +33,11 @@ const preview: Preview = {
         <APIProvider
           apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string}
         >
-          <ModalProvider>
-            <Story />
-          </ModalProvider>
+          <QueryClientProvider client={queryClient}>
+            <ModalProvider>
+              <Story />
+            </ModalProvider>
+          </QueryClientProvider>
         </APIProvider>
       </ThemeProvider>
     ),
