55266a503897f4bb8c3bd14c21d09c1f1dbbcafc
wndgur2 <wndgur76@naver.com>
2025-09-17 12:54:00 +0900

    fix: 다중 모달 오류 수정

--- src/contexts/ModalProvider.tsx
+++ src/contexts/ModalProvider.tsx
@@ -31,7 +31,10 @@ interface ModalContextType {
     _content: ReactNode,
     _closingKeyframe?: Keyframes,
   ) => Promise<ModalReturnType>
-  closeModal: (_value: ModalReturnType) => Promise<void>
+  closeModal: (
+    _value: ModalReturnType,
+    _options?: { skipBack: boolean },
+  ) => Promise<void>
   confirm: (_message: string) => Promise<ModalReturnType>
   alert: (_message: string) => Promise<ModalReturnType>
 }
@@ -43,6 +46,7 @@ function ModalProvider({ children }: { children: ReactNode }) {
 
   const openModal = useCallback(
     async (content: ReactNode, closingKeyframe: Keyframes = fadeOut) => {
+      window.history.pushState(null, '', window.location.href + '#')
       return new Promise<ModalReturnType>(resolve => {
         setModals(prev => [
           ...prev,
@@ -53,31 +57,34 @@ function ModalProvider({ children }: { children: ReactNode }) {
     [],
   )
 
-  const closeModal = useCallback(async (value: ModalReturnType) => {
-    if (window.history.state?.modal) {
-      window.history.back()
-    }
+  const closeModal = useCallback(
+    async (value: ModalReturnType, options?: { skipBack?: boolean }) => {
+      if (!options?.skipBack) {
+        window.history.back()
+      }
 
-    setModals(prev => {
-      if (!prev.length) return prev
-      const next = [...prev]
-      next[next.length - 1] = { ...next[next.length - 1], isClosing: true }
-      return next
-    })
+      setModals(prev => {
+        if (!prev.length) return prev
+        const next = [...prev]
+        next[next.length - 1] = { ...next[next.length - 1], isClosing: true }
+        return next
+      })
 
-    return new Promise<void>(resolve => {
-      setTimeout(() => {
-        setModals(prev => {
-          if (!prev.length) return prev
-          const next = [...prev]
-          const top = next[next.length - 1]
-          if (top) top.promiseResolver(value)
-          return next.slice(0, -1)
-        })
-        resolve()
-      }, FG_DURATION)
-    })
-  }, [])
+      return new Promise<void>(resolve => {
+        setTimeout(() => {
+          setModals(prev => {
+            if (!prev.length) return prev
+            const next = [...prev]
+            const top = next[next.length - 1]
+            if (top) top.promiseResolver(value)
+            return next.slice(0, -1)
+          })
+          resolve()
+        }, FG_DURATION)
+      })
+    },
+    [],
+  )
 
   const confirm = async (message: string) => {
     return openModal(<Confirm>{message}</Confirm>, slideDown)
@@ -96,7 +103,6 @@ function ModalProvider({ children }: { children: ReactNode }) {
 }
 
 function ModalRenderer({ modals }: { modals: Modal[] }) {
-  const { closeModal } = useModal()
   const modalRoot = document.getElementById('modal-root')
   if (!modalRoot || !modals.length) return null
   const topModal = modals[modals.length - 1]
@@ -105,7 +111,7 @@ function ModalRenderer({ modals }: { modals: Modal[] }) {
   return createPortal(
     <div
       css={backgroundStyle(topModal)}
-      onClick={() => closeModal(null)}
+      onClick={() => window.history.back()}
       data-testid='modal-background'
     >
       <div css={contentStyle(topModal)}>
@@ -124,10 +130,9 @@ function ModalWrapper({ children }: { children: ReactNode }) {
     if (!closeModal) return
 
     const onPopState = () => {
-      closeModal(null)
+      closeModal(null, { skipBack: true })
     }
 
-    window.history.pushState({ modal: true }, '', window.location.href + '#')
     window.addEventListener('popstate', onPopState)
 
     return () => {
