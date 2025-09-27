62d61198dc4700f36075b3a329f6097fcad81bb3
wndgur2 <wndgur76@naver.com>
2025-09-27 14:45:50 +0900

    design: css 수정

--- src/components/modal/NewBadgeModal/NewBadgeModal.tsx
+++ src/components/modal/NewBadgeModal/NewBadgeModal.tsx
@@ -1,9 +1,14 @@
 import { css, keyframes, type Theme } from '@emotion/react'
 import Realistic from 'react-canvas-confetti/dist/presets/realistic'
+import { Link } from 'react-router-dom'
 
 import type { Achievement } from '@/api'
 import Badge from '@/components/common/Badge'
+import Button from '@/components/common/Button'
+import { ROUTES } from '@/routes/ROUTES'
+import { useUserStore } from '@/store/userStore'
 import { fadeIn } from '@/styles/animation'
+import { flexGap } from '@/styles/common'
 
 type Props = Omit<Achievement, 'name' | 'obtained'>
 
@@ -14,21 +19,31 @@ export default function NewBadgeModal({ id, type }: Props) {
     particleCount: 30,
     startVelocity: 40,
   }
+
+  const associateId = useUserStore(s => s.associateId)
+
   return (
     <>
-      <div css={modalStyle}>
-        {type !== 'OPEN' ? (
-          <h2 css={hiddenSpanStyle}>히든 업적을 달성했어요!</h2>
-        ) : (
-          <h2>업적을 달성했어요!</h2>
-        )}
-        <div css={badgePlaceholderStyle}></div>
-        <div css={sunStyle}>
-          <div css={{ scale: 1.6 }}>
-            <Badge id={id} />
-          </div>
+      <div css={sunStyle}>
+        <div css={{ scale: 1.6 }}>
+          <Badge id={id} />
         </div>
-        <p css={{ lineHeight: '1em' }}>칭호를 획득했습니다.</p>
+      </div>
+      <div css={modalStyle}>
+        <div css={[flexGap(20), { height: 120, justifyContent: 'flex-end' }]}>
+          {type !== 'OPEN' ? (
+            <h2 css={hiddenSpanStyle}>히든 업적을 달성했어요!</h2>
+          ) : (
+            <h2>업적을 달성했어요!</h2>
+          )}
+        </div>
+        <div css={badgePlaceholderStyle}></div>
+        <div css={[flexGap(20), { height: 120 }]}>
+          <p>칭호를 획득했습니다.</p>
+          <Link to={ROUTES.GUEST_BOOK(associateId)}>
+            <Button label='내 방명록 가기' />
+          </Link>
+        </div>
       </div>
 
       <Realistic
@@ -69,7 +84,7 @@ export default function NewBadgeModal({ id, type }: Props) {
 
 const rise = keyframes({
   '0%': { bottom: '-20%', scale: 0.4 },
-  '100%': { bottom: '50%', scale: 1 },
+  '100%': { bottom: '49%', scale: 1 },
 })
 const spin = keyframes({
   to: { transform: 'translate(-50%, -50%) rotate(360deg)' },
@@ -77,15 +92,18 @@ const spin = keyframes({
 const spinReverse = keyframes({
   to: { transform: 'translate(-50%, -50%) rotate(-360deg)' },
 })
-const size = keyframes({
+const grow = keyframes({
   to: { width: '75vh', height: '75vh' },
+})
+
+const split = keyframes({
+  to: { gap: 120 },
 })
 
 const modalStyle = css({
   position: 'relative',
   height: '100vh',
   width: '100%',
-  paddingBottom: 34,
 
   display: 'flex',
   flexDirection: 'column',
@@ -97,9 +115,14 @@ const modalStyle = css({
   textAlign: 'center',
   overflow: 'hidden',
   backgroundColor: 'rgba(0, 0, 0, 0.85)',
+  animation: `${split} 5s ease-in-out forwards`,
 })
 
 const badgePlaceholderStyle = css({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 220,
   height: 56,
   borderRadius: 32,
@@ -145,7 +168,7 @@ const sunStyle = css({
       'radial-gradient(circle, transparent 0 70px, black 180px 280px, transparent 335px)',
     WebkitMaskImage:
       'radial-gradient(circle, transparent 0 70px, black 180px 280px, transparent 335px)',
-    animation: `${spin} 18s linear infinite, ${size} 5s ease-in-out forwards`,
+    animation: `${spin} 18s linear infinite, ${grow} 5s ease-in-out forwards`,
     filter: 'blur(0.5px)',
     opacity: 0.9,
     zIndex: -1,
@@ -166,7 +189,7 @@ const sunStyle = css({
       'radial-gradient(circle, transparent 0 90px, black 150px 0px, transparent 220px)',
     WebkitMaskImage:
       'radial-gradient(circle, transparent 0 90px, black 150px 0px, transparent 220px)',
-    animation: `${spinReverse} 120s linear infinite, ${size} 5s ease-in-out forwards`,
+    animation: `${spinReverse} 120s linear infinite, ${grow} 5s ease-in-out forwards`,
     filter: 'blur(6px)',
     opacity: 0.6,
     zIndex: -2,
