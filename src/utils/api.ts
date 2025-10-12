eddede234d50ea1fd7d9e7e4f4f32c4e7f25551a
wndgur2 <wndgur76@naver.com>
2025-10-12 16:48:18 +0900

    hotfix: timeout 1분 설정

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import axios, { AxiosError, type AxiosResponse } from 'axios'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
   },
