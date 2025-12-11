4887ad0826e9680e79d30ac28230096d9eff212b
wndgur2 <wndgur76@naver.com>
2025-12-11 19:33:46 +0900

    feat: 서버 닫혀있을 때 오류 출력

---
+++ src/api/health.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// 서버 상태 조회
+export function useServerHealth () {
+  return useQuery({
+    queryKey: ['server-health'],
+    queryFn: () =>
+      api
+        .get(`/v1/health`)
+        .then(r => r.data as { status: string }),
+  })
+}
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -14,10 +14,33 @@ export default function ErrorPage() {
 
   useThemeColor(theme.colors.bg)
 
+  let header = '문제가 발생했어요.'
+
+  let messageEl = <p>알 수 없는 오류가 발생했습니다.</p>
+  if (error.message === 'server-closed') {
+    header = '서버가 닫혀있어요.'
+    messageEl = (
+      <>
+        <span>
+          정상 이용 시간은{' '}
+          <b>
+            <i>AM 09:00 ~ AM 00:00</i>
+          </b>{' '}
+          입니다.
+        </span>
+      </>
+    )
+  } else if (error.message) {
+    messageEl = <p>error.message</p>
+  }
+
   return (
     <div css={containerStyle}>
-      <h2>문제가 발생했어요.</h2>
-      <p>{error.message}</p>
+      <h2>{header}</h2>
+      {messageEl}
+      <p>
+        문의사항은 <b>이중혁</b>에게 카톡 편하게 주세요!
+      </p>
       <div css={imageWrapperStyle}>
         <Img src={errorImage} alt='Error mascot' />
       </div>
